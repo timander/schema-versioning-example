902fc5a0333fc32fb135f70acc6d919542ce4933
Tim Andersen <timander@gmail.com>
2010-09-03 20:07:08 -0500

    cleaned up pom.xml

--- src/test/sql/testdata/02_insert_customers.sql
+++
@@ -1,2 +0,0 @@
-insert into people (first_name, last_name, address_line_1, address_line_2, phone_number, city, state, zip_code)
-  values ('Jane', 'Smith', '789 Spruce', null, '555-222-2222', 'De Soto', 'IA', '50069');
